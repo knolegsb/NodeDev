2f33525af2bea2946ed09707a288d29cf6b3a0a7
knolegsb <seon2you@hotmail.com>
2016-02-02 21:01:04 -0800

    added angular

--- app.js
+++ app.js
@@ -1,12 +1,55 @@
-﻿var express = require('express');
-var app = express();
-var port = 8000;
+﻿//var express = require('express');
+//var app = express();
+//var port = 8000;
 
-app.listen(port, function(err, res){
-    if(err) {
-        console.log("Server error");
-    }else
-    {
-        console.log("Server started");
-    }
-});
+//app.listen(port, function(err, res){
+//    if(err) {
+//        console.log("Server error");
+//    }else
+//    {
+//        console.log("Server started");
+//    }
+//});
+
+var app = angular.module("sampleApp", ['ngRoute'])
+
+.config(['$routeProvider', function ($routeProvider) {
+    $routeProvider.
+        when('/main', {
+            templateUrl: 'main.html',
+            controller: 'mainCtrl'
+        })
+        .when('/about', {
+            templateUrl: 'about.html',
+            controller: 'aboutCtrl'
+        })
+        .when('/contact', {
+            templateUrl: 'contact.html',
+            controller: 'contactCtrl'
+        })
+        .otherwise({ redirectTo: '/main' });
+}]);
+
+
+//var app = angular.module("computerApp", ['ngRoute'])
+
+//.config(['$routeProvider', function ($routeProvider) {
+//    $routeProvider.
+//        when('/main', {
+//            templateUrl: 'main.html',
+//            controller: 'mainCtrl'
+//        }).
+//        when('/about', {
+//            templateUrl: 'about.html',
+//            controller: 'aboutCtrl'
+//        }).
+//        //when('/services', {
+//        //    templateUrl: 'services.html',
+//        //    controller: 'servicesCtrl'
+//        //}).
+//        when('/contact', {
+//            templateUrl: 'contact.html',
+//            controller: 'contactCtrl'
+//        }).
+//        otherwise({ redirectTo: '/main' });
+//}]);
