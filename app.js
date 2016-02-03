f280b38001a5bc849ec6ef83817c81826b7a5e3b
knolegsb <seon2you@hotmail.com>
2016-02-02 19:31:46 -0800

    adding server

---
+++ app.js
@@ -0,0 +1,12 @@
+﻿var express = require('express');
+var app = express();
+var port = 8000;
+
+app.listen(port, function(err, res){
+    if(err) {
+        console.log("Server error");
+    }else
+    {
+        console.log("Server started");
+    }
+});
